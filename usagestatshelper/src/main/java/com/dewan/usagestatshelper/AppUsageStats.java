08cde5731b6be64841945d2966dd28dc5e121245
Richard <richarddewan@hotmail.com>
2019-11-18 17:11:33 +0700

    added method to return app list

---
+++ usagestatshelper/src/main/java/com/dewan/usagestatshelper/AppUsageStats.java
@@ -0,0 +1,22 @@
+package com.dewan.usagestatshelper;
+
+public class AppUsageStats {
+    private String packageName;
+    private long totalTimeInForeground;
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public long getTotalTimeInForeground() {
+        return totalTimeInForeground;
+    }
+
+    public void setTotalTimeInForeground(long totalTimeInForeground) {
+        this.totalTimeInForeground = totalTimeInForeground;
+    }
+}
--- usagestatshelper/src/main/java/com/dewan/usagestatshelper/UsageStatsHelper.java
+++ usagestatshelper/src/main/java/com/dewan/usagestatshelper/UsageStatsHelper.java
@@ -1,19 +1,22 @@
 package com.dewan.usagestatshelper;
 
-import android.app.AlertDialog;
+
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Build;
-import android.provider.Settings;
+import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class UsageStatsHelper {
+    private static final String TAG = "UsageStatsHelper";
 
     public static boolean getAppUsageStatsPermission(Context context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -34,7 +37,7 @@ public class UsageStatsHelper {
             UsageStatsManager usm = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
             long time = System.currentTimeMillis();
             assert usm != null;
-            List<UsageStats>  applist = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 1000, time);
+            List<UsageStats> applist = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 1000, time);
             if (applist != null && applist.size() > 0) {
 
                 for (UsageStats usageStats : applist) {
@@ -50,6 +53,29 @@ public class UsageStatsHelper {
         }
 
         return mySortedMap;
+
+    }
+
+    public static ArrayList<AppUsageStats> getAppUsageStatsList(SortedMap sortedMap) {
+        ArrayList<AppUsageStats> list = new ArrayList<>();
+        if (!sortedMap.isEmpty()) {
+            Set set = sortedMap.entrySet();
+            for (Object o : set) {
+                try {
+                    Map.Entry entry = (Map.Entry) o;
+                    long key = (long) entry.getKey();
+                    UsageStats value = (UsageStats) entry.getValue();
+                    AppUsageStats appUsageStats = new AppUsageStats();
+                    appUsageStats.setPackageName(value.getPackageName());
+                    appUsageStats.setTotalTimeInForeground(value.getTotalTimeInForeground());
+                    list.add(appUsageStats);
+                } catch (Exception er) {
+                    Log.e(TAG, "getAppUsageStatsList:" + er.getLocalizedMessage());
+                }
+
+            }
+        }
+        return list;
 
     }
 }
